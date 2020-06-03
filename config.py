e0e1a36064def0f7d9b053ec8e8ae1999b1fa785
Emma Bobae Kong <36050671+EmmaK0822@users.noreply.github.com>
2020-06-03 11:19:37 -0700

    Delete config.py
    confidentials

--- config.py
+++
@@ -1,7 +0,0 @@
-# Twitter API Key
-consumer_key = "mFd3RgJC68pRJkvx9KDaUCONu"
-consumer_secret = "1g8H54ctPqNgAreS8rZblmGygTUHk1JR7jqwcK7VSNvjQ9PrfC"
-access_token = "1004891522523807744-ikGO5kQ6Ycqp5Pvoufm2zGrZhT9zqG"
-access_token_secret = "eBNDbcHjG4qUYS1SRpHdR96dbfmPbYi7Y0QNhBOJoYkL5"
-
-weather_api_key = "25bc90a1196e6f153eece0bc0b0fc9eb"
